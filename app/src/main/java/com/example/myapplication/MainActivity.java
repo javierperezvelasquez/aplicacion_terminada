665a1cc19a7b0fceee2f23204d422c7c31889987
javierperezvelasquez <javierperezvelazquezz@gmail.com>
2019-10-06 20:38:30 -0300

    proyecto inicial partes 2.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,32 +6,71 @@ import androidx.constraintlayout.widget.Constraints;
 
 import android.content.Intent;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img;
+    Bitmap bitmap;
+    String URLIMAGE = "https://autolab.com.co/wp-content/uploads/2019/02/Iconos-Atributos_Mesa-de-trabajo-1-copia-4.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
+
+        new GetImageFromURL(img).execute(URLIMAGE);
 
 
-
-        //Poner icono en el action bar
-    getSupportActionBar().setDisplayShowHomeEnabled(true);
-    getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+    //metodo el boton agregar
+    public void Agregarcliente(View view) {
+        Intent Agregarcliente = new Intent(this, ActivityAgregar.class);
+        startActivity(Agregarcliente);
 
     }
 
-    //metodo el boton agregar
-    public void Agregarcliente(View view ){
-        Intent Agregarcliente = new Intent( this,ActivityAgregar.class);
-        startActivity(Agregarcliente);
+        public class GetImageFromURL extends AsyncTask<String, Void, Bitmap> {
+        ImageView imgV;
+
+        public GetImageFromURL(ImageView imgV) {
+            this.imgV = imgV;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            String urldisplay = url[0];
+            bitmap = null;
+            try {
+                InputStream srt = new java.net.URL(urldisplay).openStream();
+                bitmap = BitmapFactory.decodeStream(srt);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imgV.setImageBitmap(bitmap);
+
+
+            //Poner icono en el action bar
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        }
 
 
     }
